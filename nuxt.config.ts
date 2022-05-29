78fdffccf4b1e144af2a70390ccfdad06bbdeff1
Scott Rod <djmtype@gmail.com>
2022-05-29 16:33:27 -0400

    Try pnpm instead

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,10 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
 
-	nitro: {
-    prerender: {
-      routes: ['/playlists']
-    }
-  }
+	// nitro: {
+  //   prerender: {
+  //     routes: ['/playlists']
+  //   }
+  // }
 
 })
