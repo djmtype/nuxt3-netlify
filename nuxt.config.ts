d3210d07a99a2c357582aac73042c18abfbfe84d
Scott Rod <djmtype@gmail.com>
2022-05-29 13:45:17 -0400

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,10 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
 
+	nitro: {
+    prerender: {
+      routes: ['/playlists']
+    }
+  }
+
 })
